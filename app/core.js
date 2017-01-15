cf6b93ca956b39e96c7c86960f4fa81add6284c0
Stéphane Dion <divers@sdion.net>
2017-01-15 07:40:44 +0100

    -

--- app/core.js
+++ app/core.js
@@ -255,7 +255,7 @@ function urlBase64ToUint8Array(base64String) {
     const rawData = window.atob(base64);
     const outputArray = new Uint8Array(rawData.length);
 
-    for(let i = 0; i < rawData.length; ++i) {
+    for(var i = 0; i < rawData.length; ++i) {
         outputArray[i] = rawData.charCodeAt(i);
     }
     return outputArray;
