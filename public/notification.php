f9c18d87d4854277e31e452595fd900c830e5a3d
Stéphane Dion <divers@sdion.net>
2020-10-09 19:32:52 +0200

    -

--- public/notification.php
+++ public/notification.php
@@ -14,7 +14,7 @@ $result = [];
 if(isset($postdata['endpoint']) && isset($postdata['public_key']) && isset($postdata['authentication_secret'])) {
     $apiKeys = [
         'VAPID' => [
-            'subject' => 'mailto:divers@sdion.net',
+            'subject' => 'Stéphane Dion - play',
             'publicKey' => 'BOQ0NVE+w6/fJYX3co0H8w1zre4T7uT4ssMOLfWd1rWzMmuowf7NC/pz5X9roHNTu2Qhvt0pAcKnvjnM3Aw/ssA=',
             'privateKey' => '9+1EJiRiJiI8/YdV2Jts94ZJr9k2sCXdNIGypZh0oTo=',
         ],
